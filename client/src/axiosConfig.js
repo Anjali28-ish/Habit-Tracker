6b0acbd69173493cdfd3ff939dddecaabe53df9f
Anjali28-ish <anjalikumarimishra28@gmail.com>
2026-02-14 01:05:49 +0530

    Update axios baseURL to include '/api'

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://habit-tracker-n1b1.onrender.com/",
+  baseURL: "https://habit-tracker-n1b1.onrender.com/api",
 });
 
 instance.interceptors.request.use((config) => {
